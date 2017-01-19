b9f463f9bba2b7f558358d12f1c8728136cc78e9
Nick Kasten <kastencode@gmail.com>
2017-01-19 00:57:27 -0600

    bot now listens for pizza tweets and displays hashtags on console

--- bot.js
+++ bot.js
@@ -5,15 +5,30 @@ dotenv.load()
 var secret = {
   consumer_key:         process.env.CONSUMER_KEY,
   consumer_secret:      process.env.CONSUMER_SECRET,
-  access_token_key:         process.env.ACCESS_TOKEN,
+  access_token_key:     process.env.ACCESS_TOKEN,
   access_token_secret:  process.env.ACCESS_TOKEN_SECRET
 }
 var Twitter = new TwitterPackage(secret)
 
+/* TEST TWEET.. UNCOMMENT THIS TO POST WHEN BOT IS RUN
+ *
 Twitter.post('statuses/update', {status: 'I like Pizza!'}, function(error, tweet, response) {
   if (error) {
     console.log(error)
   }
   console.log(tweet)        // log the tweet body
   console.log(response)     // log the raw response object
+})
+*/
+
+Twitter.stream('statuses/filter', {track: '#pizza'}, function(stream) {
+  stream.on('data', function(tweet) {
+    var hashtags = tweet.entities.hashtags.map(function(entry) {
+      return entry.text
+    })
+    console.log(hashtags)
+  })
+  stream.on('error', function(error) {
+    console.log(error)
+  })
 })
