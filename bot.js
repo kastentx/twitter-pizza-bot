23d2b2d8e839953e092b767bbab92135e007b82e
Nick Kasten <kastencode@gmail.com>
2017-01-18 23:29:30 -0600

    bot able to post tweets

--- bot.js
+++ bot.js
@@ -1,12 +1,19 @@
-var twit = require('twit')
+var TwitterPackage = require('twitter')
 var dotenv = require('dotenv')
-
 dotenv.load()
 
-var Twitter = new Twit({
+var secret = {
   consumer_key:         process.env.CONSUMER_KEY,
   consumer_secret:      process.env.CONSUMER_SECRET,
-  access_token:         process.env.ACCESS_TOKEN,
+  access_token_key:         process.env.ACCESS_TOKEN,
   access_token_secret:  process.env.ACCESS_TOKEN_SECRET
-})
+}
+var Twitter = new TwitterPackage(secret)
 
+Twitter.post('statuses/update', {status: 'I like Pizza!'}, function(error, tweet, response) {
+  if (error) {
+    console.log(error)
+  }
+  console.log(tweet)        // log the tweet body
+  console.log(response)     // log the raw response object
+})
